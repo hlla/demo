7e1fd3a37a8705bc50aef124e84304e2ba29b4e2
jixicai <jixicai@yy.com>
2018-05-07 10:40:20 +0800

    提交代码

--- TestComponent/src/main/java/example/com/testcomponent/TestComponentActivity.java
+++ TestComponent/src/main/java/example/com/testcomponent/TestComponentActivity.java
@@ -1,10 +1,16 @@
 package example.com.testcomponent;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import java.lang.reflect.Method;
@@ -17,6 +23,11 @@ import butterknife.OnClick;
 
 public class TestComponentActivity extends Activity {
     private static final String TAG = "TestComponent1_Activity";
+    public static final String ACTION = "android.intent.action.mystaticreceiver";
+    @BindView(R.id.bindservice)
+    Button bindservice;
+    @BindView(R.id.broadcast)
+    Button broadcast;
     private WeakHashMap<TestComponentActivity, String> weakHashMap = new WeakHashMap<>();
     @BindView(R.id.query)
     Button query;
@@ -30,12 +41,13 @@ public class TestComponentActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_component);
         ButterKnife.bind(this);
-        getContentResolver().registerContentObserver(URIField.ACCOUNT_URI, true, new ContentObserver(null) {
-            @Override
-            public void onChange(boolean selfChange) {
-                Log.d(TAG, "onChange selfChange=" + selfChange);
-            }
-        });
+        getContentResolver().registerContentObserver(URIField.ACCOUNT_URI, true, new
+                ContentObserver(null) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        Log.d(TAG, "onChange selfChange=" + selfChange);
+                    }
+                });
     }
 
     @OnClick(R.id.query)
@@ -45,7 +57,7 @@ public class TestComponentActivity extends Activity {
             public void run() {
                 try {
                     Class cr = getClassLoader().loadClass("android.content.ContentResolver");
-                    Method method = cr.getMethod("acquireProvider", android.net.Uri.class);
+                    Method method = cr.getMethod("acquireProvider", Uri.class);
                     Object transport = method.invoke(getContentResolver(), URIField
                             .ACCOUNT_URI);
                     Log.d(TAG, "onQueryClicked: " + transport + "  class=" + transport.getClass());
@@ -92,5 +104,33 @@ public class TestComponentActivity extends Activity {
                 getContentResolver().update(URIField.ACCOUNT_URI, new ContentValues(), null, null);
             }
         }.start();
+    }
+
+    @OnClick({R.id.bindservice, R.id.broadcast})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bindservice:
+                Intent intent = new Intent();
+                intent.setClassName("letv.com.testanr11", "letv.com.testanr.MyService");
+                boolean result = bindService(intent, new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName name, IBinder service) {
+                        Log.d(TAG, "onServiceConnected: ");
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName name) {
+                        Log.d(TAG, "onServiceDisconnected: ");
+
+                    }
+                }, BIND_AUTO_CREATE);
+                Log.d(TAG, "onBindServiceClicked: end result=" + result);
+                break;
+            case R.id.broadcast:
+                Intent intent1 = new Intent(ACTION);
+                intent1.setFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+                sendBroadcast(intent1);
+                break;
+        }
     }
 }
